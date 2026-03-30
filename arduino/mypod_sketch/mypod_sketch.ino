d70114220a877d9db4c4a9bc5ffde5728f11e683
felix <felix@laptop>
2026-03-30 21:57:45 +0200

    Changes: Added indexing of sd Card

--- arduino/mypod_sketch/mypod_sketch.ino
+++ arduino/mypod_sketch/mypod_sketch.ino
@@ -1,74 +1,103 @@
-#include <WiFiNINA.h>
-#include <utility/wifi_drv.h>
 #include <SPI.h>
-#include <SD.h>
+#include <SdFat.h>
 
-int bluePin=27;
-int redPin=26;
-int greenPin=25;
-int volDownPin=0;
+const int volDownPin=16;
+const int sdPin = SDCARD_SS_PIN;
 
-int volDownButtonState = 0;
-bool buttonPressed=false;
-
-File root;
+SdFat sd;
+SdFile root;
+File myFile;
+String rootPath = "/";
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
   while(!Serial);
-
-  Serial.print("Initialising SD reader ... ");
-  if(!SD.begin(10)){
-    Serial.println("error");
-    while(true);
+  
+  Serial.print("Initializing SD card...");
+  // see if the card is present and can be initialized:
+  if (!sd.begin(sdPin)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
   }
-  root = SD.open("/");
-  Serial.println("sucess!");
   pinMode(volDownPin, INPUT);
-  WiFiDrv::pinMode(greenPin, OUTPUT);
-  WiFiDrv::pinMode(redPin, OUTPUT);
-  WiFiDrv::pinMode(bluePin, OUTPUT);
+  Serial.println("indexing...");
+  processDirectory("/");
+  Serial.println("Done");
 }
 
 void loop() {
-  volDownButtonState = digitalRead(volDownPin);
-  if(volDownButtonState == HIGH){
-    Serial.println("down");
-    
-  }
-  Serial.println("up");
-  // put your main code here, to run repeatedly:
-  //ledBlinkOnce(1000, bluePin);
-  //ledBlinkOnce(1000, redPin);
-  //ledBlinkOnce(1000, greenPin);
+  int volDownButtonState = digitalRead(volDownPin);
+  int read = analogRead(A1);
+  float voltage = read;
+
+  //AudioEngine
+
+  //InputManager
+
+  //PlayerState
+
 }
 
-void ledBlinkOnce(int time, int pin){
-  WiFiDrv::digitalWrite(pin, HIGH); //Turn On Green light
-  delay(time);
-  WiFiDrv::digitalWrite(pin, LOW); //Turn Off Green light
-  delay(time);
-}
-void printDirectory(File dir, int numTabs) {
-  while (true) {
-    File entry =  dir.openNextFile();
-    if (! entry) {
-      // no more file
-      break;
-    }
-    for (uint8_t i = 0; i < numTabs; i++) {
-      Serial.print('\t');
-    }
-    Serial.print(entry.name());
-    if (entry.isDirectory()) {
-      Serial.println("/");
-      printDirectory(entry, numTabs + 1);
-    } else {
-      // files have sizes, directories do not
-      Serial.print("\t\t");
-      Serial.println(entry.size(), DEC);
-    }
-    entry.close();
+void processDirectory(const char* path) {
+  FsFile dir;
+  if (!dir.open(path)) {
+    Serial.print("Failed to open dir: ");
+    Serial.println(path);
+    return;
   }
+
+  // build index file path
+  char indexPath[128];
+  if (strcmp(path, "/") == 0) {
+    snprintf(indexPath, sizeof(indexPath), "/index.txt");
+  } else {
+    snprintf(indexPath, sizeof(indexPath), "%s/index.txt", path);
+  }
+
+  FsFile indexFile;
+  if (!indexFile.open(indexPath, O_WRITE | O_CREAT | O_TRUNC)) {
+    Serial.print("Failed to create: ");
+    Serial.println(indexPath);
+    dir.close();
+    return;
+  }
+
+  FsFile file;
+
+  while (file.openNext(&dir, O_RDONLY)) {
+    char name[64];
+    file.getName(name, sizeof(name));
+
+    // skip index file itself
+    if (strcmp(name, "index.txt") == 0) {
+      file.close();
+      continue;
+    }
+
+    if (file.isDirectory()) {
+      // write directory name with '/'
+      indexFile.print(name);
+      indexFile.println("/");
+
+      // build subdirectory path
+      char subPath[128];
+      if (strcmp(path, "/") == 0) {
+        snprintf(subPath, sizeof(subPath), "/%s", name);
+      } else {
+        snprintf(subPath, sizeof(subPath), "%s/%s", path, name);
+      }
+
+      file.close();              // important!
+      processDirectory(subPath); // recurse
+    } else {
+      // write file name
+      indexFile.println(name);
+      file.close();
+    }
+  }
+
+  indexFile.close();
+  dir.close();
 }
